41664bc7ec2399a0b05337cd58cec826e0b772f9
bertoramos <ramossanchezalberto@gmail.com>
2025-10-25 20:51:23 +0100

    fix: update timestamp format in output directory path

--- src/save_data.py
+++ src/save_data.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def save_dataframe(title, df):
     # Crea una carpeta con la fecha actual en data/output
-    output_dir = Path("data/output") / datetime.now().strftime("%Y-%m-%d")
+    output_dir = Path("data/output") / datetime.now().strftime("%H-%M-%S_%Y-%m-%d")
     output_dir.mkdir(parents=True, exist_ok=True)
 
     # Guarda el DataFrame en un archivo Excel con el título como nombre de archivo
